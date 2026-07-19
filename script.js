17fd732b379b8d1eae30e3ac7ae91cd5691ea095
Shubham sonkar <sonkarshubham9336@gmail.com>
2026-07-19 22:14:45 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+const toggle = document.getElementById("theme-toggle");
+
+toggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    if(document.body.classList.contains("dark")){
+        toggle.textContent = "☀️";
+    } else {
+        toggle.textContent = "🌙";
+    }
+});
+const toggle = document.getElementById("theme-toggle");
+
+if(localStorage.getItem("theme") === "dark"){
+    document.body.classList.add("dark");
+    toggle.textContent = "☀️";
+}
+
+toggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    if(document.body.classList.contains("dark")){
+        localStorage.setItem("theme","dark");
+        toggle.textContent="☀️";
+    }else{
+        localStorage.setItem("theme","light");
+        toggle.textContent="🌙";
+    }
+});
